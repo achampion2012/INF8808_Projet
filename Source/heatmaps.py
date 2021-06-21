4aa5808a6fc90ad6f7dbc8bf23d1b6159aa4c176
Mohamed Amine Merzouk <mamerzouk@protonmail.com>
2021-06-21 16:19:25 -0400

    Add the code file for the heatmaps

---
+++ Source/heatmaps.py
@@ -0,0 +1,19 @@
+import plotly.express as px
+
+def get_heatmap_week(df):
+    fig = px.imshow(fbg, color_continuous_scale="magma", width=1000)
+    fig.update_layout(xaxis_title="Semaine", yaxis_title="Heure")
+    fig.update_traces(hovertemplate="<br>".join([
+            "<b> Semaine: </b> %{x}",
+            "<b> Heure: </b> %{y}h00 - %{y}h59",
+            "<b> Interactions: </b> %{z}"]))
+    return fig
+
+def get_heatmap_weekday(df):
+    fig = px.imshow(fbg, color_continuous_scale="magma", width=300)
+    fig.update_layout(xaxis_title="Jour", yaxis_title="Heure")
+    fig.update_traces(hovertemplate="<br>".join([
+            "<b> Jour: </b> %{x}",
+            "<b> Heure: </b> %{y}h00 - %{y}h59",
+            "<b> Interactions: </b> %{z}"]))
+    return fig
