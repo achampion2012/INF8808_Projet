af4b411d789bef3710c06495f1cef176b8de9245
Mohamed Amine Merzouk <mamerzouk@protonmail.com>
2021-06-21 16:19:44 -0400

    Add the heatmaps section in app.py

--- Source/app.py
+++ Source/app.py
@@ -66,4 +66,42 @@ app.layout = html.Div(
         # ])
 
 
+        # Heatmaps
+        #   df = pd.read_csv('../src/assets/data/facebookCanada2020.csv', index_col=0)
+        #   df = preproc.FilterData(df)
+        #   df = preproc.datecreated_to_datetime(df)
+        #   df_hm = preproc.datecreated_to_datetime(df)
+        #   df_hm = preproc.week_hour_weekday(df_hm)
+        #   df_hm_week = preproc.week_hour_publication(df_hm)
+        #   df_hm_weekday = weekday_hour_publication(df_hm)
+
+        df_hm_week = pd.read_csv('../src/assets/data/hm_week.csv', index_col=0)
+        df_hm_weekday = pd.read_csv('../src/assets/data/hm_weekday.csv', index_col=0)
+
+        fig_hm_week = heatmaps.get_heatmap_week(df_hm_week)
+        fig_hm_weekday = heatmaps.get_heatmap_weekday(df_hm_weekday)
+
+        html.Div(children=[
+            html.Header("Number of publications for each week of 2020"),
+            dcc.Graph(figure=fig_hm_week, id='fig_hm_week',
+                      config=dict(
+                          showTips=False,
+                          showAxisDragHandles=False,
+                          doubleClick=False,
+                          displayModeBar=False)),
+            #html.Footer("First figure footer")
+            ])
+
+            html.Div(children=[
+                html.Header("Number of publications for each weekday of 2020"),
+                dcc.Graph(figure=fig_hm_weekday, id='fig_hm_weekday',
+                          config=dict(
+                              showTips=False,
+                              showAxisDragHandles=False,
+                              doubleClick=False,
+                              displayModeBar=False)),
+                #html.Footer("First figure footer")
+                ])
+
+
     ])
