52b7c288a9e10651ec01dc4e206800aa8255be1b
Rshukr <70924173+Rshukr@users.noreply.github.com>
2021-06-28 16:07:07 -0400

    Update visualisation2_1.py

--- Source/visualisation2_1.py
+++ Source/visualisation2_1.py
@@ -3,16 +3,21 @@ import plotly.express as px
 def draw_scatter_followers(data):
     fig = px.scatter(data, x="followers", y="reaction", range_y=[0, 60000], range_x=[0, 10000000], opacity=0.8, color='Color')
     fig.update_layout(title="Nombre moyen de réactions par post selon le nombre de followers de la page")
-    fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='rgba(0,0,0,0)',
-    title_x=0.5,
-    font_color='rgb(59,89,152)'
+    fig.update_layout(
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        title_x=0.5,
+        font_color='rgb(59,89,152)'
+    )
     return fig
 
 def draw_barchart_type(data):
     fig = px.bar(cdf.groupby(["type"]).median(), y="reaction")
-    fig.update_layout(yaxis={"title":"Nombre de réactions médian"}, xaxis={'categoryorder':'array', 'categoryarray':["Live Video", "Photo", "Video", "Status", "Album", "Link"], "title":"Type de publication"})
-    plot_bgcolor='rgba(0,0,0,0)',
-    title_x=0.5,
-    font_color='rgb(59,89,152)'
+    fig.update_layout(
+        yaxis={"title":"Nombre de réactions médian"}, 
+        xaxis={'categoryorder':'array', 'categoryarray':["Live Video", "Photo", "Video", "Status", "Album", "Link"], "title":"Type de publication"},
+        plot_bgcolor='rgba(0,0,0,0)',
+        title_x=0.5,
+        font_color='rgb(59,89,152)'
+    )
     return fig
