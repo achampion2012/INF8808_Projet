a6b15db90558a61be8f8d2fd307276f6acd91ada
clemigg <83023493+clemigg@users.noreply.github.com>
2021-06-28 20:48:06 -0400

    Update visualisation0.py

--- Source/visualisation0.py
+++ Source/visualisation0.py
@@ -13,23 +13,26 @@ def draw_average_lang():
     x = ["Anglais", "Français", "Autre", "Arabe", "Espagnol"]
     y = [2456276, 601355, 385855, 118331, 37882 ]
     
-
-    fig = px.bar(x=x, y=y, 
-    title='Répartition des publications selon la langue en 2020',  
-    labels={'x':'Langue de publication', 'y': 'Nombre de publications'} )
-    fig.update_traces(marker_color='rgb(59,89,152)')
-    fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='rgba(0,0,0,0)',
-    title_x=0.5,
-    font_color='rgb(59,89,152)'),
-    
-     hover_template = (
+    hover_template = (
         "<b>Type de publication :</b> %{x}<br>" +
         "<b>Nombre de publication :</b> %{y}<br>" + 
         "<extra></extra>"
     )
+    
+    fig = px.bar(x=x, y=y, 
+    title='Répartition des publications selon la langue en 2020',  
+    labels={'x':'Langue de publication', 'y': 'Nombre de publications'} )
+    fig.update_traces(marker_color='rgb(59,89,152)',
+                      hovertemplate=hover_template
+    )
+    
+    fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    title_x=0.5,
+    font_color='rgb(59,89,152)')
+    )
         
-    fig.update_traces(hovertemplate=hover_template)
+    fig.update_traces()
 
     return fig
     
