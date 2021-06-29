8132505fe2e9527431694ed389ca9e0a08046796
achampion2012@hotmail.com <achampion2012@hotmail.com>
2021-06-28 21:37:05 -0400

    Update tile xaxes and yaxes

--- Source/visualisation2_1.py
+++ Source/visualisation2_1.py
@@ -1,7 +1,8 @@
 import plotly.express as px
-
+ColorScatter = ["#0084ff","#44bec7","#ffc300","#fa3c4c","#d696bb"]
 def draw_scatter_followers(data):
-    fig = px.scatter(data, x="followers", y="reaction", range_y=[0, 60000], range_x=[0, 10000000], opacity=0.8, color='Color')
+    fig = px.scatter(data, x="followers", y="reaction", range_y=[0, 60000], range_x=[0, 10000000], opacity=0.8, color='Color',
+    color_discrete_sequence=ColorScatter)
     fig.update_layout(title="Nombre moyen de réactions par post selon le nombre de followers de la page")
     fig.update_layout(
         paper_bgcolor='rgba(0,0,0,0)',
@@ -17,7 +18,8 @@ def draw_scatter_followers(data):
     )
     
     fig.update_traces(hovertemplate=hover_template)
-    
+    fig.update_xaxes(title="Nombre de followers")
+    fig.update_yaxes(title="Nombre de réaction")
     return fig
 
 def draw_barchart_type(data):
